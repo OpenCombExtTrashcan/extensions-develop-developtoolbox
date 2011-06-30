784eeea7e2accef7437c8a2f91341009561a710e
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-30 10:29:44 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/develop/developtoolbox/trunk@76 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/Index.php
+++ class/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace oc\ext\developtoolbox ;
 
+use jc\fs\FSOIterator;
+
 use jc\system\ClassLoader;
 
 use oc\base\FrontFrame;
@@ -21,16 +23,20 @@ class Index extends Controller
 	public function process()
 	{
 		// 反射 class namespace
-		$arrNamespacesInfo = $this->reflectNamespace( $this->application()->classLoader() ) ;
+		list($arrNamespacesInfo,$arrControllerClasses) = $this->scanExtensions( $this->application()->classLoader() ) ;
+		
 		$this->view->variables()->set('sDefineNamespacesCode',json_encode($arrNamespacesInfo)) ;
+		$this->view->variables()->set('sDefineAllControllerClassesCode',json_encode($arrControllerClasses)) ;
 
 		//
 		 
 	}
 	
-	public function reflectNamespace(ClassLoader $aClassLoader)
+	public function scanExtensions(ClassLoader $aClassLoader)
 	{
 		$arrNamespacesInfo = array() ;
+		$arrControllerClasses = array() ;
+		
 		foreach( $aClassLoader->namespaceIterator() as $sNamespace)
 		{
 			$sFolderPath = $aClassLoader->namespaceFolder($sNamespace) ;
@@ -45,22 +51,54 @@ class Index extends Controller
 			}
 			$arrPackages = array_values($arrPackages) ;
 			
-			if( count($arrPackages)>=3 and $arrPackages[0]=='oc' and $arrPackages[1]=='ext' )
+			if( count($arrPackages)<3 or $arrPackages[0]!='oc' or $arrPackages[1]!='ext' )
 			{
-				$sExtensionName = $arrPackages[2] ;
+				continue ;
 			}
-			else 
-			{
-				$sExtensionName = null ;
-			}
+			$sExtensionName = $arrPackages[2] ;
 			
 			$arrNamespacesInfo[$sNamespace] = array(
 				'folder' => $sFolderPath ,
 				'extension' => $sExtensionName ,
 			) ;
+			
+			$aIter = new FSOIterator($sFolderPath,(FSOIterator::FLAG_DEFAULT&(~FSOIterator::RETURN_PATH))|FSOIterator::RETURN_SUBPATH) ;
+			$aIter->filters()->add(function ($sPath){
+				if( strstr($sPath,'.svn/')===false )
+				{
+					return $sPath ;
+				}
+			}) ;
+			
+			// 遍历所有的扩展类包
+			foreach($aIter as $sSubFolder)
+			{
+				// package
+				if( $aIter->isDir() )
+				{
+					$arrNamespacesInfo[$sNamespace.'\\'.str_replace('//','\\',$sSubFolder)] = array(
+						'folder' => $sFolderPath.'/'.$sSubFolder ,
+						'extension' => $sExtensionName ,
+					) ;
+				}
+				
+				// class
+				else 
+				{
+					$sFilename = $aIter->filename() ;
+					$sClass = substr( $sFilename,0,strpos($sFilename,'.') ) ;
+					
+					$sFullClass = $sNamespace . '\\' . (dirname($sSubFolder)=='.'? '': (dirname($sSubFolder).'\\')) . $sClass ;
+					
+					if( class_exists($sFullClass) and is_subclass_of($sFullClass,'jc\\mvc\\controller\\IController') )
+					{
+						$arrControllerClasses[] = $sFullClass ;
+					}
+				}
+			}
 		}
 		
-		return $arrNamespacesInfo ;
+		return array($arrNamespacesInfo,$arrControllerClasses) ;
 	}
 }
 

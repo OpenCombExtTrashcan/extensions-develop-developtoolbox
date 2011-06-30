b274e7da42ac4a93aff5d6b9f05cee81ae78858a
kongyuan <kongyuan@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-30 05:18:26 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/develop/developtoolbox/trunk@71 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- ui/js/developtoolbox.js
+++ ui/js/developtoolbox.js
@@ -14,12 +14,12 @@ jQuery(function () {
 	}
 	
 	//数据对象,测试用
-	var treeData ={"name":"testname","class":"testclass" , "children":[]};
+	treeData ={"name":"testname","class":"testclass" , "children":[]};
 	
 	//获得node的层级,顶级为0,顶级的子node就是1,孙node就是2,依次类推
 	function getLevel(aNode){
 		var classNames = aNode.attr("class").split(' ');
-	  for(key in classNames) {
+	  for(var key in classNames) {
 	    if(typeof(classNames[key])=="string" && classNames[key].match("level_")) {
 	    	var arrLevel = classNames[key].split('_');
 	      return Number(arrLevel[1]);
@@ -36,6 +36,7 @@ jQuery(function () {
 			aNode.find(".tr_indent").css("width" ,16*nLevel+"px" );
 		}	
 	}
+	
 	
 	//选中
 	function setSelected(aNode){
@@ -54,22 +55,38 @@ jQuery(function () {
 	
 	//属性页面切换
 	function getPropertyPage(aNode){
+		var sNodeType = getNodeType(aNode);
+		//如果是widget的属性页,把属性附表先隐藏
+		if(sNodeType == "widget"){
+			jQuery(".widget_propertys").hide(0).appendTo($("#widget_property_store"));
+		}
+		//处理属性通用页面
 		jQuery( ".propertys" ).hide(0);
 		var aPropertyPage = jQuery( "#" + getNodeType(aNode) + "_property" );
 		aPropertyPage.show(0);
 		//得到数据
 		var aData = aNode.data("property");
 		getProperties(aPropertyPage,aData);
+		//if(sNodeType == "widget"){
+		//	var sWidgetPropertyPageId = "#"+$("#widget_property #widget_classname").val() +'_property';
+		//	jQuery(sWidgetPropertyPageId).appendTo($("#other_property")).show(0);
+		//}
 	}
+	
 	
 	//还原属性栏内容
 	function getProperties(aPropertyPage,aData){
-		//得到widget
+		//先看看是不是widget类型,如果是widget类型,那么先组合附表单
+		if( aData["objectClass"] == "widget" && aData["classname"] != undefined){  //处理select属性附属页面
+			widgetTypeChange(aData["classname"]);
+		}
+		
+		//得到属性表单中的widget
 		var arrProperty = getPropertyWidget(aPropertyPage);
 		//复原widget的值
-		$.each(arrProperty, function(i,n){
-			var sArgId = $(n).attr("id");
-			var sArgName = $(n).attr("id").split("_")[1];
+		jQuery.each(arrProperty, function(i,n){
+			var sArgId = jQuery(n).attr("id");
+			var sArgName = jQuery(n).attr("id").split("_")[1];
 			if(aData!=undefined){
 				var sValue = aData[sArgName];
 			}
@@ -78,9 +95,9 @@ jQuery(function () {
 				aArgWidget[0].checked = sValue;
 			}else if( sArgName!="submit"){
 				if(sValue != undefined){
-					$(n).val(sValue);
+					jQuery(n).val(sValue);
 				}else{
-					$(n).val(null);
+					jQuery(n).val(null);
 				}
 			}
 		});
@@ -89,6 +106,19 @@ jQuery(function () {
 	//返回一个数组,包含propertypage的可提交控件的对象数组,比如input和select控件
 	function getPropertyWidget(aPropertyPage){
 		return aPropertyPage.find("input,select");
+	}
+	
+	//widget 的类型(比如;select,checkbox,text等)变化时触发
+	function widgetTypeChange(widgetClass){
+		var sWidgetClass = "";
+		if(widgetClass == null){
+			sWidgetClass = $("#widget_property #widget_classname").val();
+		}else{
+			sWidgetClass = widgetClass;
+		}
+		var sWidgetPropertyPageId = "#"+ sWidgetClass +'_property';
+		jQuery(".widget_propertys").appendTo($("#widget_property_store")).hide(0);
+		jQuery(sWidgetPropertyPageId).appendTo($("#other_property")).show(0);											   
 	}
 	
 	//新建node(tr)
@@ -106,7 +136,7 @@ jQuery(function () {
 		}
 		
 		var aNewNode = jQuery("#"+newNodeId);
-		var aNewNodeProperty = {"type":sNewType,"children":[]}
+		var aNewNodeProperty = {"objectClass":sNewType,"children":[]}
 		aNewNode.data("property",aNewNodeProperty);
 		if(aParent!=null){
 			var aParentProperty = aParent.data("property");
@@ -138,7 +168,8 @@ jQuery(function () {
 	
 	//获得node的type
 	function getNodeType(aNode){
-		return aNode.find("td span").attr("class");
+		//alert(aNode.find("span").attr("class"));
+		return aNode.find("span").attr("class");
 	}
 	
 	//左侧按钮按照选中的tr的类型切换可用状态,如果没有aNode参数,typeclass=noselected,意为没有任何被选中标签
@@ -161,7 +192,7 @@ jQuery(function () {
 	function getParent(aNode){
 		var arrClasses = aNode.attr("class").split(" ");
 		var sParentId = "";
-		for(key in arrClasses){
+		for(var key in arrClasses){
 			if(typeof(arrClasses[key])=="string" && arrClasses[key].match(childClassPre)){
 				sParentId = arrClasses[key].split("_")[2];
 			}
@@ -169,7 +200,7 @@ jQuery(function () {
 		if(sParentId == ""){
 			return false; //没有父node
 		}
-		return $("#"+sParentId);
+		return jQuery("#"+sParentId);
 	}
 	
 	//删除数组元素
@@ -183,6 +214,12 @@ jQuery(function () {
 	  arr.length-=1;
 	  return arr;
   	}
+	
+	//转义 . ,初期目的是为了防止用文件名做ID的时候 . 被误读成css选择器的. 
+	function escapeId(sOldId){
+		var sNewId = sOldId.replace(/\./,"-");
+		return sNewId;
+	}
 	
 	//左侧按钮功能部分
 	jQuery(".addButtons").click(function(){
@@ -203,37 +240,77 @@ jQuery(function () {
 		setSelected(jQuery(this));
 	});
 	
+	//删除按钮
+	$("#deleteBtn").click(function(){
+		if(!confirm('确实要删除这个对象吗? \n\n所有的子对象都会被删除!!')){
+			return ;	
+		}
+		var aSelected = $(".selected");
+		if(aSelected <= 0){
+			return;
+		}
+		var aSelectedProperty = aSelected.data("property");
+		var aSelectedParentProperty ;
+		//删除父node的children中的记录
+		var aSelectedParent = getParent(aSelected);
+		if(aSelectedParent == false){//如果用户想删除的对象是顶级对象,则删除treedata中的children的数据,即吧treedata当做父对象
+			aSelectedParentProperty = treeData;
+		}else{
+			aSelectedParentProperty = aSelectedParent.data("property");
+		}
+		for(var key in aSelectedParentProperty["children"]){
+			if(aSelectedParentProperty["children"][key] == aSelectedProperty){
+				//delete aSelectedParentProperty["children"][key];
+				removeElementByIndex(key,aSelectedParentProperty["children"]);
+			}
+		}
+		//删除对象,连同数据
+		aSelected.remove();
+	});
+	
 	//属性提交
 	jQuery(".submitBtn").live("click",function(){
-		var aSelected = $(".selected");	
-		var sSubmitBtnId = $(this).attr("id");	
+		 var fdsfsdtreeData = treeData;
+		var aSelected = jQuery(".selected");	
+		var sSubmitBtnId = jQuery(this).attr("id");	
 		var sSubmitType = sSubmitBtnId.split("_")[0];
-		var arrProperties = getPropertyWidget($("#"+sSubmitType+"_property"));
+		var arrProperties = getPropertyWidget(jQuery("#"+sSubmitType+"_property"));
 		var dataObject = {};
 		if(aSelected.data("property")!=undefined){
 			dataObject=aSelected.data("property");
+			//除了children意外,清除所有的数据
+			for(var key in dataObject){
+				if(key != "children" && key!="objectClass"){
+					delete dataObject[key];
+				}
+			}
 		}
 		//取值
-		$.each( arrProperties , function(i, n){
-			var sArgName = $(n).attr("id").split("_")[1];
+		jQuery.each( arrProperties , function(i, n){
+			var sArgName = jQuery(n).attr("id").split("_")[1];
 			var sArgValue = "";
 			if(n.type == "checkbox" || n.type == "radio" ){
 				if(n.checked){
 					sArgValue = n.checked;
 				}
 			}else{
-				sArgValue = $(n).val();
+				sArgValue = jQuery(n).val();
 			}
 			if(sArgName!="submit"){
 				dataObject[sArgName] = sArgValue;
 			}
 		});
 		//修正对象列表中的ID和text
-		var name = $("#"+sSubmitType+"_property .object_name").val();
-		aSelected.attr("id",name).find("td b").text(name);
+		var name = jQuery("#"+sSubmitType+"_property .object_name").val();
+		aSelected.attr("id",escapeId(name)).find("td b").text(name);
 		//数据保存到tr对象
 		//aSelected.data("property",dataObject);
-		treeData = treeData;
+		 fdsfsdtreeData = treeData;
 	});
 	//json  toJSON(objectData);  jQuery.evalJSON(
+												 
+	//widget 类型选择
+	jQuery("#widget_classname").live("change",function(){
+		widgetTypeChange(null);												   
+	});
 });
